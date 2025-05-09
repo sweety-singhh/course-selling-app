2f7807888211a4734cb1c5f46ac340f3b3c72b7b
Sweety <sweetysingh91221@gmail.com>
2025-05-10 00:16:47 +0530

    Update config.js

--- backend/config.js
+++ backend/config.js
@@ -2,8 +2,8 @@ import dotenv  from "dotenv";
 dotenv.config();
 const JWT_USER_PASSWORD= process.env.JWT_USER_PASSWORD;
 const JWT_ADMIN_PASSWORD= process.env.JWT_ADMIN_PASSWORD;
-const STRIPE_SECRET_KEY =
-"sk_test_51RMhRw4cEytmVpwER6HCJa8XNZ1dp6NLT6doC79Arv1Qyb7b3VjvW3wxvbrZE0DHbU3CT3sJkWwis9Jl9hb6BPvE00KPijV0KR"
+// const STRIPE_SECRET_KEY =
+// "sk_test_51RMhRw4cEytmVpwER6HCJa8XNZ1dp6NLT6doC79Arv1Qyb7b3VjvW3wxvbrZE0DHbU3CT3sJkWwis9Jl9hb6BPvE00KPijV0KR"
 
 export default {
     JWT_USER_PASSWORD,
